e419f9204b469f4cbd300dd3c1c3812b08a4c97a
gichu wil <gichuwil@gmail.com>
2021-05-31 16:16:27 +0300

    added vue 2, tailwind, post-css 7

---
+++ vue-tailwind/src/pages/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="bg-gray-100 p-96">
+    
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import HelloWorld from '@/components/HelloWorld.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    HelloWorld
+  }
+}
+</script>
